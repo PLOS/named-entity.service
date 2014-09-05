670f469968c39e4ba71d60a6ab06aaa3d4f26c28
Jono Finger <jfinger@plos.org>
2014-09-05 15:23:19 -0700

    PS-90: Added plos heading to url.

--- makePentahoPojoJar.sh
+++ makePentahoPojoJar.sh
@@ -4,8 +4,9 @@ cd target/classes
 jar cvf ../ned-pojos.jar org/plos/namedentity/api/* org/plos/namedentity/validate/Validatable.class
 cd ..
 
-# make symlinks (examples below)
+# make symlinks or copy the jar to external directories (examples below)
 
 #ln -s `pwd`/ned-pojos.jar ~/applications/pentaho/lib
+cp ned-pojos.jar ~/applications/pentaho/lib
 #ln -s `pwd`/ned-pojos.jar ~/src/named-entity.etl/update-ned.plugin/lib
-
+cp ned-pojos.jar ~/src/named-entity.etl/update-ned.plugin/lib
