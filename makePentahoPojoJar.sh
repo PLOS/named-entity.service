7ee6781214f530f329c950cf58854ff572e70443
Matthew Bowen <mbowen@plos.org>
2014-10-01 11:14:45 -0700

    Add META-INF/services when packaging pojos for deployment to Pentaho.

--- makePentahoPojoJar.sh
+++ makePentahoPojoJar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# tip: run "package" phase to ensure META-INF/services gets deployed to classes dir
+
 cd target/classes
-jar cvf ../ned-pojos.jar org/plos/namedentity/api/* org/plos/namedentity/validate/Validatable.class
+jar cvf ../ned-pojos.jar org/plos/namedentity/api/* org/plos/namedentity/validate/Validatable.class META-INF
 cd ..
 
 # make symlinks or copy the jar to external directories (examples below)
