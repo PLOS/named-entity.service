fd7e81893cea387577a9e3fb9abb045c8477efa1
Jono <jono@foodnotblogs.com>
2015-04-21 21:52:29 -0700

    Made better

--- docker_community/run.sh
+++ docker_community/run.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-### set up database
+BUILD_DIR=/root
 
-sleep 5	# TODO: poll database for when ready instead of sleeping
+# wait for DB to be ready for schema
 
 MYSQL="mysql --default-character-set=utf8 -h ${MYSQL_HOSTNAME} -u root -p${MYSQL_ROOT_PASSWORD}"
 
+${MYSQL} < ${BUILD_DIR}/ned-schema.mysql.sql
+MYSQL_NOT_CONNECTING=$?
+while [ $MYSQL_NOT_CONNECTING -ne 0 ] ; do
+    sleep 1;
+    ${MYSQL} < ${BUILD_DIR}/ned-schema.mysql.sql
+    MYSQL_NOT_CONNECTING=$?
+    echo -e "\nDatabase (${MYSQL_HOSTNAME}) not ready for schema ... waiting"
+done;
+
+# set up db
+
 echo -e "\nCreating NED Schema"
-${MYSQL} < /mysql_schema/ned-schema.mysql.sql
+${MYSQL} < ${BUILD_DIR}/ned-schema.mysql.sql
 
 echo -e "\nSeeding NED Schema"
-${MYSQL} < /mysql_schema/ned-data.mysql.sql
+${MYSQL} < ${BUILD_DIR}/ned-data.mysql.sql
 
 echo 'SELECT User FROM mysql.user' | ${MYSQL}
 
@@ -19,12 +30,11 @@ echo "CREATE USER '${MYSQL_NED_USER}' IDENTIFIED BY '${MYSQL_NED_PASSWORD}'" | $
 echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_NED_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES" | ${MYSQL}
 echo "Finished creating user."
 
-### set up tomcat
+# set up tomcat
 
 echo Deleting contents of webapps/
 rm -rf ${CATALINA_HOME}/webapps/*
 
-BUILD_DIR=/root
 NED_SVC_WAR=ned.war
 
 echo Checking that WAR exists
@@ -39,6 +49,8 @@ else
   exit 1
 fi
 
+# wait for db to be ready for service
+
 MYSQL_CMD="mysql -h ${MYSQL_HOSTNAME} -u ${MYSQL_NED_USER} -p${MYSQL_NED_PASSWORD} ${MYSQL_DATABASE}"
 
 $MYSQL_CMD -e 'exit'
@@ -47,15 +59,13 @@ while [ $MYSQL_NOT_CONNECTING -ne 0 ] ; do
     sleep 1;
     $MYSQL_CMD -e 'exit'
     MYSQL_NOT_CONNECTING=$?
-    echo -e "\nDatabase (${MYSQL_HOSTNAME}) not ready ... waiting"
+    echo -e "\nDatabase (${MYSQL_HOSTNAME}) not ready for API ... waiting"
 done;
 
 echo -e "\nDatabase (${MYSQL_HOSTNAME}) ready!"
 
 echo `ip addr`
 
-### bring up service
+# bring up service
 
 ${CATALINA_HOME}/bin/catalina.sh run
-
-
--- docker_community/up.sh
+++ docker_community/up.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
-cp ../target/named-entity-service-*.war nedsvc
+DIR=`pwd`
+
+WAR="../target/named-entity-service-*.war"
+
+BUILDDIR="build"
+
+if [ -f $WAR ]; then
+	echo found .war file: $WAR
+else
+	echo Building .war
+	cd ..
+	./ned.sh install
+	cd $DIR
+fi
+
+pwd
+echo Prepping build directory
+
+# TODO: build context.xml from config directory
+
+mkdir -p $BUILDDIR
+
+
+if [ ! -f "${BUILDDIR}/ned.war" ]; then
+	cp $WAR "${BUILDDIR}/ned.war"
+fi
+
+cp ../src/main/resources/ned-*.mysql.sql build
+
+ls build
+
 docker-compose rm --force -v && docker-compose build && docker-compose up
