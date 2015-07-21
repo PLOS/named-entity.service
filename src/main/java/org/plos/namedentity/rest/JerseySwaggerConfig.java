2777f9d6a0a02b992483bb31bcbbbdb4488417dd
Jono Finger <jfinger@plos.org>
2015-07-21 09:32:37 -0700

    PS-826: Trying to do a full upgrade to swagger spec 2.0 to no avail.

--- src/main/java/org/plos/namedentity/rest/JerseySwaggerConfig.java
+++ src/main/java/org/plos/namedentity/rest/JerseySwaggerConfig.java
@@ -1,26 +1,40 @@
 package org.plos.namedentity.rest;
 
-import com.wordnik.swagger.config.ConfigFactory;
-import com.wordnik.swagger.model.ApiInfo;
+import io.swagger.jaxrs.config.BeanConfig;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class JerseySwaggerConfig extends HttpServlet {
 
-  static {
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
 
-// ConfigFactory.config().setBasePath("http://www.foo.com/");
-    ApiInfo info = new ApiInfo(
-        null,//"Named Entity Database API", // title
-        null, // description
-        null, // TOS URL
-        null, // Contact
-        null, // license
-        null // license URL
-    );
-
-    ConfigFactory.config().setApiInfo(info);
-    //ConfigFactory.config().setApiVersion("v1");
-
+    BeanConfig beanConfig = new BeanConfig();
+    beanConfig.setVersion("1.0.2");
+    beanConfig.setSchemes(new String[]{"http"});
+    beanConfig.setHost("localhost:8080");
+    beanConfig.setBasePath("/");
+//    beanConfig.setResourcePackage("io.swagger.resources");
+    beanConfig.setScan(true);
   }
+
+//  static {
+//
+//// ConfigFactory.config().setBasePath("http://www.foo.com/");
+//    ApiInfo info = new ApiInfo(
+//        null,//"Named Entity Database API", // title
+//        null, // description
+//        null, // TOS URL
+//        null, // Contact
+//        null, // license
+//        null // license URL
+//    );
+//
+//    ConfigFactory.config().setApiInfo(info);
+//    //ConfigFactory.config().setApiVersion("v1");
+//
+//  }
 }
