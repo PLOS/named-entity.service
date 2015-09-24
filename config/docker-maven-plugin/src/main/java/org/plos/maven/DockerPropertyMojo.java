abe0d4e34117dcc8837db23faf5a4f08861d79b8
Matthew Bowen <mbowen@plos.org>
2015-09-24 15:58:53 -0700

    PS-1111: refactor plugin to lookup docker commands on PATH instead of hardcoded path (bleck!).

--- config/docker-maven-plugin/src/main/java/org/plos/maven/DockerPropertyMojo.java
+++ config/docker-maven-plugin/src/main/java/org/plos/maven/DockerPropertyMojo.java
@@ -3,10 +3,6 @@ package org.plos.maven;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Paths;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -17,7 +13,6 @@ public class DockerPropertyMojo extends AbstractMojo
 {
   private static final String DOCKER_IP_HOST = "docker.ip.host";
   private static final String DOCKER_INSTANCE_NAME = "neddb";
-
 
   @Parameter( property = "docker-properties.skip", defaultValue = "false", alias = "docker-properties.skip" )
   private boolean skip;
@@ -30,26 +25,44 @@ public class DockerPropertyMojo extends AbstractMojo
     }
 
     getLog().info("Resolving Docker Host");
+
+    String dockerIp = getDockerIp(true);
+    if (dockerIp != null) {
+      setSystemProperty(DOCKER_IP_HOST, dockerIp);
+    } else {
+      dockerIp = getDockerIp(false);
+      if (dockerIp != null) {
+        setSystemProperty(DOCKER_IP_HOST, dockerIp);
+      }
+    }
+  }
+
+  private void setSystemProperty(String key, String value) {
+      getLog().info(key+"="+value);
+      System.setProperty(key, value);
+  }
+
+  private String getDockerIp(boolean dockerMachine) {
     try {
       Process process = null;
-      if (Files.exists(Paths.get("/usr/local/bin/docker-machine"), LinkOption.NOFOLLOW_LINKS)) {
-        process = new ProcessBuilder("docker-machine", "ip", DOCKER_INSTANCE_NAME).start();
-      }
-      else if (Files.exists(Paths.get("/usr/bin/docker"), LinkOption.NOFOLLOW_LINKS)) {
+      if (dockerMachine) {
+        process = new ProcessBuilder("docker-machine", "ip").start();
+      } else {
         process = new ProcessBuilder("docker", "inspect", "--format",
-                                     "'{{ .NetworkSettings.IPAddress }}'", DOCKER_INSTANCE_NAME).start();
+                                    "'{{ .NetworkSettings.IPAddress }}'", DOCKER_INSTANCE_NAME).start();
       }
       BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
       String dockerIp = in.readLine();
       int pExitValue = process.waitFor();
       if (pExitValue == 0 && !isEmptyOrBlank(dockerIp)) {
-        dockerIp = dockerIp.replace("'","");
-        getLog().info(DOCKER_IP_HOST+"="+dockerIp);
-        System.setProperty(DOCKER_IP_HOST, dockerIp);
+        // ip string is delimited by single quotes. remove them.
+        return dockerIp.replace("'","");
       }
-    } catch (Exception e) {
-      getLog().error("Problem resolving docker host.", e);
     }
+    catch (java.io.IOException | InterruptedException ex) {
+      // an io exception will be thrown if program wasn't found. gobble it.
+    }
+    return null;
   }
 
   private boolean isEmptyOrBlank(String s) {
@@ -58,6 +71,11 @@ public class DockerPropertyMojo extends AbstractMojo
 
   public static void main (String [] args)
   {
-    try { new DockerPropertyMojo().execute(); } catch (Exception e) { e.printStackTrace(); }
+    try {
+      new DockerPropertyMojo().execute();
+      System.out.println(DOCKER_IP_HOST+"="+System.getProperty(DOCKER_IP_HOST));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
