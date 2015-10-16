1d3b69d22d806f1c91b911e90bc027076420d47b
Jono Finger <jfinger@plos.org>
2015-10-16 16:43:31 -0700

    Clean up ruby sample

--- client/sample.rb
+++ client/sample.rb
@@ -1,7 +1,7 @@
 require 'swagger_client'
 
 SwaggerClient.configure { |c| [
-   c.debugging = true, c.host='http://akita:akita@localhost:8080',
+   c.debugging = false, c.host='http://akita:akita@localhost:8080',
    c.username = 'akita', c.password = 'akita'] }
 
 # TODO: get basic auth to work in the header https://github.com/swagger-api/swagger-codegen/issues/1407
@@ -17,10 +17,10 @@ serviceapi = SwaggerClient::ServiceApi.new(apiclient)
 individualsapi = SwaggerClient::IndividualsApi.new(apiclient)
 typeclassesapi = SwaggerClient::TypeclassesApi.new(apiclient)
 
-serviceapi.config
+print serviceapi.config
 
 # serviceapi.errorcodes
 
 typeclassesapi.list
 
-individualsapi.read_individual(53).emails
+print individualsapi.read_individual(53).emails[0].emailaddress
