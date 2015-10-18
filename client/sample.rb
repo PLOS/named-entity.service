6cc9bd9cffc0ca7421f491277ff01670901d5f9a
Jono <jono@foodnotblogs.com>
2015-10-18 09:16:38 -0700

    Moved basic auth params from url into headers in ruby client

--- client/sample.rb
+++ client/sample.rb
@@ -1,17 +1,14 @@
 require 'swagger_client'
 
 SwaggerClient.configure { |c| [
-   c.debugging = false, c.host='http://akita:akita@localhost:8080',
-   c.username = 'akita', c.password = 'akita'] }
+   c.debugging = false, c.host='http://localhost:8081',
+   c.username = 'dev', c.password = 'dev'] }
 
-# TODO: get basic auth to work in the header https://github.com/swagger-api/swagger-codegen/issues/1407
 header = {'Authorization'=> SwaggerClient.configure.basic_auth_token}
-
-print header
 
 apiclient = SwaggerClient::ApiClient.new
 
-apiclient.update_params_for_auth!(header, {}, ['header'])
+apiclient.default_headers = header
 
 serviceapi = SwaggerClient::ServiceApi.new(apiclient)
 individualsapi = SwaggerClient::IndividualsApi.new(apiclient)
@@ -21,6 +18,6 @@ print serviceapi.config
 
 # serviceapi.errorcodes
 
-typeclassesapi.list
+print typeclassesapi.list
 
-print individualsapi.read_individual(53).emails[0].emailaddress
+# print individualsapi.read_individual(53).emails[0].emailaddress
