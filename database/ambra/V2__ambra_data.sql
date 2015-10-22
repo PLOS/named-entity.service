4756b9dd4d6c104b899ae767c11bb86e55b0a9f0
Matthew Bowen <mbowen@plos.org>
2015-10-22 13:17:31 -0700

    PS-1129: add user to ambra so is in-sync with ned db. modified jdbc url.

---
+++ database/ambra/V2__ambra_data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO ambra.userProfile(userProfileID, userProfileURI, authId, created, lastModified, password)
+    VALUES(1, 'info:doi/10.1371/profile/8f828914-552b-406e-820a-7490e4f2e64d', 'HMN9D5JQELQOG3272W56LSA4M0RBA6WV',
+           NOW(), NOW(), '16fc0c7af52f8a807f1debe0748d95013ce2a911b2a23672578da9a3343e2a8fda9072');
