69ee1e8bd1363a7768aa3ca3193252584ad46ece
Jono <jono@foodnotblogs.com>
2015-10-23 23:04:57 -0700

    Working on CLI

--- client/cli.py
+++ client/cli.py
@@ -1,20 +1,20 @@
 """Python command line client for NED
 
 Usage:
-  cli.py API METHOD [<arguments>...]
+  cli.py API [METHOD] [<arguments>...]
 
 APIS:
   service
   typeclasses
+  individuals
+  organizations
+  institutionsearch
 
 Examples:
   service config
-  service errorcodes
   typeclasses list
-  typeclasses read 23
-  individuals read_individual 23
+  typeclasses read 12
   individuals find_individuals email emailaddress john@doe.com
-  individuals get_addresses 1
   individuals get_address 1 1
 
 Options:
@@ -24,28 +24,41 @@ Options:
 """
 
 import swagger_client
+import inspect
 from docopt import docopt
 
 swagger_client.configuration.username = 'akita'
 swagger_client.configuration.password = 'akita'
 
-apiclient = swagger_client.ApiClient('http://localhost:8080', header_name="Authorization", header_value=swagger_client.configuration.get_basic_auth_token())
-
-apis = {}
-apis['service'] = swagger_client.ServiceApi(apiclient)
-apis['typeclasses'] = swagger_client.TypeclassesApi(apiclient)
-apis['individuals'] = swagger_client.IndividualsApi(apiclient)
-apis['organizations'] = swagger_client.OrganizationsApi(apiclient)
-apis['institutionsearch'] = swagger_client.InstitutionsearchApi(apiclient)
+apiclient = swagger_client.ApiClient('http://localhost:8080/vx', header_name="Authorization", header_value=swagger_client.configuration.get_basic_auth_token())
 
 _method = lambda m, o : getattr(o,m)
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='1.x')
+    args = docopt(__doc__)
 
-    api = args['API']
+    apiname = args['API']
     method = args['METHOD']
     methodargs = args['<arguments>']
-    print(args)
+    c = getattr(swagger_client, apiname.capitalize() + "Api")
 
-    print (_method(method, apis[api])(methodargs))
+    # print(args)
+
+    api = c(apiclient)
+
+    if not method:
+
+        members = inspect.getmembers(api, predicate=inspect.ismethod)
+
+        print "Methods:"
+
+        for member in members:
+            if member[0].find('__') == 0:
+                continue
+            m = getattr(c, member[0])
+            args = inspect.getargspec(m).args
+            del args[0]
+            print ("  " + apiname + " " + member[0] + "  " + (' '.join(['<'+x+'>' for x in args])))
+
+    else:
+        print(_method(method, api)(*methodargs))
