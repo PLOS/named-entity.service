f5c2678a1b759c6f8742e734e8d660915acd584b
Jono <jono@foodnotblogs.com>
2015-10-23 23:46:43 -0700

    Updated CLI to start allowing pushing data

--- client/cli.py
+++ client/cli.py
@@ -1,7 +1,7 @@
 """Python command line client for NED
 
 Usage:
-  cli.py API [METHOD] [<arguments>...]
+  cli.py [-i FILE] API [METHOD] [<arguments>...]
 
 APIS:
   service
@@ -16,19 +16,21 @@ Examples:
   typeclasses read 12
   individuals find_individuals email emailaddress john@doe.com
   individuals get_address 1 1
+  individuals create_individual -i composite.json
 
 Options:
+  -i FILE       The input file (or use '-' for stdin)
   -h --help     Show this screen.
-  --version     Show version.
-
 """
 
 import swagger_client
 import inspect
+import sys
 from docopt import docopt
 
 swagger_client.configuration.username = 'akita'
 swagger_client.configuration.password = 'akita'
+swagger_client.configuration.debug = True
 
 apiclient = swagger_client.ApiClient('http://localhost:8080/vx', header_name="Authorization", header_value=swagger_client.configuration.get_basic_auth_token())
 
@@ -40,9 +42,11 @@ if __name__ == '__main__':
     apiname = args['API']
     method = args['METHOD']
     methodargs = args['<arguments>']
+    infile = args["-i"]
+
     c = getattr(swagger_client, apiname.capitalize() + "Api")
 
-    # print(args)
+    print(args)
 
     api = c(apiclient)
 
@@ -61,4 +65,14 @@ if __name__ == '__main__':
             print ("  " + apiname + " " + member[0] + "  " + (' '.join(['<'+x+'>' for x in args])))
 
     else:
-        print(_method(method, api)(*methodargs))
+        if infile:
+            if infile == '-':
+                fh = sys.stdin
+            else:
+                fh = open(infile, "r")
+            body = fh.read()
+            print (body)
+            fh.close()
+            print(_method(method, api)(*methodargs, body=body))
+        else:
+            print(_method(method, api)(*methodargs))
