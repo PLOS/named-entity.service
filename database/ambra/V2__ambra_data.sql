6e87f9c085c51fd949fee841977cadba4b9b6744
Matthew Bowen <mbowen@plos.org>
2015-10-28 15:35:21 -0700

    PS-1129: better define seed data.

--- database/ambra/V2__ambra_data.sql
+++ database/ambra/V2__ambra_data.sql
@@ -1,4 +1,7 @@
 
-INSERT INTO ambra.userProfile(userProfileID, userProfileURI, authId, created, lastModified, password)
-    VALUES(1, 'info:doi/10.1371/profile/8f828914-552b-406e-820a-7490e4f2e64d', 'HMN9D5JQELQOG3272W56LSA4M0RBA6WV',
-           NOW(), NOW(), '16fc0c7af52f8a807f1debe0748d95013ce2a911b2a23672578da9a3343e2a8fda9072');
+/* pre: ned schema hasn't been created yet (by flyway migrations) */
+INSERT INTO ambra.userProfile (userProfileID, userProfileURI, authId, realName, givenNames, 
+                               surName, email, displayName, biography, created, lastModified, password)
+    VALUES (1, 'info:doi/10.1371/profile/09dcb739-00c2-497a-8470-fc0bc4722b0e', UUID(),
+        'Cosmo Kramer','Cosmo','Kramer','ckramer@plos.org','ckramer','a short bio', NOW(), NOW(), 
+        'f953d98b896b3739bf925346cffb4e2e9b5e724d14bc66bc6562672944ae21d1f76bf904416af9ce52ddff3d9b46b447a4d1d6e17318299e967d0362d0df0ad6');
