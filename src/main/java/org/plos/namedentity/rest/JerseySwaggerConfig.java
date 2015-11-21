a74e46692ec88d626b74bca13b3f482de1b9dd34
Jono Finger <jfinger@plos.org>
2015-11-20 17:55:01 -0800

    Attempting workaround for swagger https issue

--- src/main/java/org/plos/namedentity/rest/JerseySwaggerConfig.java
+++ src/main/java/org/plos/namedentity/rest/JerseySwaggerConfig.java
@@ -1,6 +1,5 @@
 package org.plos.namedentity.rest;
 
-import io.swagger.models.Scheme;
 import io.swagger.models.Swagger;
 import io.swagger.models.auth.BasicAuthDefinition;
 
@@ -8,8 +7,6 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import java.util.ArrayList;
-import java.util.List;
 
 public class JerseySwaggerConfig extends HttpServlet {
 
@@ -20,11 +17,11 @@ public class JerseySwaggerConfig extends HttpServlet {
         Swagger swagger = new Swagger();
 
         swagger.securityDefinition("basic", new BasicAuthDefinition());
-        List<Scheme> schemes = new ArrayList<Scheme>();
-        schemes.add(Scheme.HTTP);
-        schemes.add(Scheme.HTTPS);
-
-        swagger.setSchemes(schemes);
+//        List<Scheme> schemes = new ArrayList<>();
+////        schemes.add(Scheme.HTTP);
+//        schemes.add(Scheme.HTTPS);
+//
+//        swagger.setSchemes(schemes);
 
         context.setAttribute("swagger", swagger);
     }
