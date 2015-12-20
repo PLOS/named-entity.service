70754f17d54350015ffdc62b3d668099b7b2621b
Jono <jono@foodnotblogs.com>
2015-12-20 12:59:06 -0800

    working on new swagger CLI client

--- client/cli.py
+++ client/cli.py
@@ -23,16 +23,16 @@ Options:
   -h --help     Show this screen.
 """
 
-import swagger_client
+import ned_client
 import inspect
 import sys
 from docopt import docopt
 
-swagger_client.configuration.username = 'akita'
-swagger_client.configuration.password = 'akita'
-swagger_client.configuration.debug = True
+ned_client.configuration.username = 'akita'
+ned_client.configuration.password = 'akita'
+# ned_client.configuration.debug = True
 
-apiclient = swagger_client.ApiClient('http://localhost:8080/vx', header_name="Authorization", header_value=swagger_client.configuration.get_basic_auth_token())
+apiclient = ned_client.ApiClient('http://localhost:8080/v0')
 
 _method = lambda m, o : getattr(o,m)
 
@@ -44,9 +44,9 @@ if __name__ == '__main__':
     methodargs = args['<arguments>']
     infile = args["-i"]
 
-    c = getattr(swagger_client, apiname.capitalize() + "Api")
+    c = getattr(ned_client, apiname.capitalize() + "Api")
 
-    print(args)
+    # print(args)
 
     api = c(apiclient)
 
@@ -54,7 +54,7 @@ if __name__ == '__main__':
 
         members = inspect.getmembers(api, predicate=inspect.ismethod)
 
-        print "Methods:"
+        # print "Methods:"
 
         for member in members:
             if member[0].find('__') == 0:
@@ -62,7 +62,7 @@ if __name__ == '__main__':
             m = getattr(c, member[0])
             args = inspect.getargspec(m).args
             del args[0]
-            print ("  " + apiname + " " + member[0] + "  " + (' '.join(['<'+x+'>' for x in args])))
+            # print ("  " + apiname + " " + member[0] + "  " + (' '.join(['<'+x+'>' for x in args])))
 
     else:
         if infile:
@@ -71,7 +71,7 @@ if __name__ == '__main__':
             else:
                 fh = open(infile, "r")
             body = fh.read()
-            print (body)
+            # print (body)
             fh.close()
             print(_method(method, api)(*methodargs, body=body))
         else:
