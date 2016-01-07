061e1c1b13db19c8a7a8c885c1ba7750a6b58d24
Jono <jono@foodnotblogs.com>
2016-01-06 16:41:58 -0800

    PS-1253 Working on test data

--- database/test/V9_1__ps1253.sql
+++ database/test/V9_1__ps1253.sql
@@ -1,2 +1,32 @@
--- INSERT INTO namedEntities.alerts ()
--- TODO: populate this
+SELECT gt.id INTO @alertTypeJournalIdVar
+  FROM namedEntities.globalTypes gt
+  JOIN namedEntities.typeDescriptions td ON gt.typeid = td.id
+ WHERE td.description='Alert Types' AND gt.shortDescription='Journal';
+
+SELECT gt.id INTO @alertTypeSearchIdVar
+  FROM namedEntities.globalTypes gt
+  JOIN namedEntities.typeDescriptions td ON gt.typeid = td.id
+ WHERE td.description='Alert Types' AND gt.shortDescription='Search';
+
+SELECT gt.id INTO @alertFreqTypeIdVar
+  FROM namedEntities.globalTypes gt
+  JOIN namedEntities.typeDescriptions td ON gt.typeid = td.id
+ WHERE td.description='Alert Frequency' AND gt.shortDescription='weekly';
+
+SELECT gt.id INTO @journalTypeIdVar
+  FROM namedEntities.globalTypes gt
+  JOIN namedEntities.typeDescriptions td ON gt.typeid = td.id
+ WHERE td.description='Journal' AND gt.shortDescription='PLOS Biology';
+
+SELECT gt.id INTO @srcTypeIdVar
+   FROM namedEntities.globalTypes gt
+   JOIN namedEntities.typeDescriptions td ON gt.typeid = td.id
+  WHERE td.description='Source Applications' AND gt.shortDescription='Ambra';
+
+SELECT id INTO @consumerIdVar FROM namedEntities.consumers WHERE name = 'test';
+
+INSERT INTO namedEntities.alerts(id, nedId, typeId, frequencyTypeId, journalTypeId, name, query, sourceTypeId, createdBy, lastModifiedBy)
+    VALUES (1, 1, @alertTypeJournalIdVar, @alertFreqTypeIdVar, @journalTypeIdVar, "alert one", "", @sourceTypeId, @consumerIdVar, @consumerIdVar);
+
+INSERT INTO namedEntities.alerts(id, nedId, typeId, frequencyTypeId, journalTypeId, name, query, sourceTypeId, createdBy, lastModifiedBy)
+    VALUES (1, 1, @alertTypeSearchIdVar, @alertFreqTypeIdVar, @journalTypeIdVar, "alert two", "{\"query\":\"goes here\"}", @sourceTypeId, @consumerIdVar, @consumerIdVar);
