5602bff905172b0ca2db2731ade1ff93ed0cdd96
Jono Finger <jfinger@plos.org>
2016-02-09 15:34:32 -0800

    PS-1275 Updated migration script to more intelligently remove (thanks Matt) foreign keys

--- database/migrations/V10__ps1275.sql
+++ database/migrations/V10__ps1275.sql
@@ -9,14 +9,22 @@ DELETE FROM alerts WHERE typeId IN (
 
 -- remove columns from alerts table
 
---SELECT constraint_name INTO @typeConstraint from information_schema.key_column_usage where constraint_schema='namedEntities' AND table_name='alerts' and column_name='typeId';
---SELECT constraint_name INTO @journalTypeConstraint from information_schema.key_column_usage where constraint_schema='namedEntities' AND table_name='alerts' and column_name='journalTypeId';
---ALTER TABLE alerts DROP FOREIGN KEY @typeConstraint;
---ALTER TABLE alerts DROP FOREIGN KEY @journalTypeId;
+SELECT constraint_name INTO @typeConstraint from information_schema.key_column_usage where constraint_schema='namedEntities' AND table_name='alerts' and column_name='typeId';
+SET @dropFkeySql = CONCAT('ALTER TABLE alerts DROP FOREIGN KEY ', @typeConstraint);
+PREPARE stmt1 FROM @dropFkeySql;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
+SELECT constraint_name INTO @typeConstraint from information_schema.key_column_usage where constraint_schema='namedEntities' AND table_name='alerts' and column_name='journalTypeId';
+SET @dropFkeySql = CONCAT('ALTER TABLE alerts DROP FOREIGN KEY ', @typeConstraint);
+PREPARE stmt1 FROM @dropFkeySql;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
 
 -- hack since I cant get the above to work with name resolution
-ALTER TABLE alerts DROP FOREIGN KEY alerts_ibfk_2;
-ALTER TABLE alerts DROP FOREIGN KEY alerts_ibfk_5;
+-- ALTER TABLE alerts DROP FOREIGN KEY alerts_ibfk_2;
+-- ALTER TABLE alerts DROP FOREIGN KEY alerts_ibfk_5;
 
 
 ALTER TABLE alerts DROP COLUMN typeId, DROP COLUMN journalTypeId;
