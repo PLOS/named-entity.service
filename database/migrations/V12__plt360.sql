e00c51121f69dee62c3777250d7d6cfba862b657
eaknowledge <Everett Aknowledge>
2016-08-31 15:46:15 -0700

    fixing failures oh no

--- database/migrations/V12__plt360.sql
+++ database/migrations/V12__plt360.sql
@@ -1,17 +1,17 @@
-
 USE namedEntities;
 
 --turn off FK
 SET foreign_key_checks = 0;
 
 Use namedEntities;
---Get globalType ID 
-Select id INTO @glblid from globalTypes where shortDescription = 'Taiwan (Province of China)'
+--Get globalType ID
+SET @glbid =(select id from globalTypes where shortDescription = "Taiwan \(Province of China\)");
 
--- Update globalType By ID 
-SET @updatGlobalSql= ('UPDATE globalTypes SET shortDescription="Taiwan" where ID=', @glblid);
+SET @newVal='Taiwan';
+SET @updatGlobalSql='UPDATE globalTypes SET shortDescription=@newVal where ID=@glblid';
 PREPARE stmt1 FROM @updatGlobalSql;
 EXECUTE stmt1;
 DEALLOCATE PREPARE stmt1;
 
 SET foreign_key_checks = 1;
+
