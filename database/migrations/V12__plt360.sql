7a71883dac25ea7ad56107e2e07860bd10ceade0
eaknowledge <Everett Aknowledge>
2016-08-31 14:04:58 -0700

    Made suggested changes to harden migration script

--- database/migrations/V12__plt360.sql
+++ database/migrations/V12__plt360.sql
@@ -3,6 +3,14 @@ USE namedEntities;
 
 SET foreign_key_checks = 0;
 
-UPDATE globalTypes SET shortDescription="Taiwan" where ID=507 AND typeId=17 AND typeCode='TW';
+Use namedEntities;
+--Get globalType ID 
+Select id INTO @glblid from globalTypes where shortDescription LIKE 'Taiwan (Province of China)'
+
+-- Update globalType By ID 
+SET @updatGlobalSql= ('UPDATE globalTypes SET shortDescription="Taiwan" where ID=', @glblid);
+PREPARE stmt1 FROM @updatGlobalSql;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
 
 SET foreign_key_checks = 1;
