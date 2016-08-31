171371c42c19b5e50168801acc42826c4690eb4c
eaknowledge <Everett Aknowledge>
2016-08-31 14:31:20 -0700

    After review migration script

--- database/migrations/V12__plt360.sql
+++ database/migrations/V12__plt360.sql
@@ -6,7 +6,7 @@ SET foreign_key_checks = 0;
 
 Use namedEntities;
 --Get globalType ID 
-Select id INTO @glblid from globalTypes where shortDescription LIKE 'Taiwan (Province of China)'
+Select id INTO @glblid from globalTypes where shortDescription = 'Taiwan (Province of China)'
 
 -- Update globalType By ID 
 SET @updatGlobalSql= ('UPDATE globalTypes SET shortDescription="Taiwan" where ID=', @glblid);
