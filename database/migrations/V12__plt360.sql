1efc4743c30707c6d2ebfdf96681d28cdfb864c5
eaknowledge <Everett Aknowledge>
2016-08-31 16:05:26 -0700

    Removed delimit change prepare statement format for UPDATE

--- database/migrations/V12__plt360.sql
+++ database/migrations/V12__plt360.sql
@@ -5,12 +5,11 @@ SET foreign_key_checks = 0;
 
 Use namedEntities;
 --Get globalType ID
-SET @glbid =(select id from globalTypes where shortDescription = "Taiwan \(Province of China\)");
+SET @glbid =(select id from globalTypes where shortDescription = 'Taiwan (Province of China)');
 
 SET @newVal='Taiwan';
-SET @updatGlobalSql='UPDATE globalTypes SET shortDescription=@newVal where ID=@glblid';
-PREPARE stmt1 FROM @updatGlobalSql;
-EXECUTE stmt1;
+PREPARE stmt1 FROM 'UPDATE namedEntities.globalTypes SET shortDescription=? where ID=?';
+EXECUTE stmt1 USING @newVal, @glbid;
 DEALLOCATE PREPARE stmt1;
 
 SET foreign_key_checks = 1;
