4d9b24cb0fb2ffc72bcb0a8941aa2791fc3e77a3
Jono <jono@foodnotblogs.com>
2016-09-14 16:29:00 -0700

    PLT-344 Updating database test seeds

--- database/test/V2_1__ned_test_data.sql
+++ database/test/V2_1__ned_test_data.sql
@@ -6,10 +6,14 @@ SELECT id INTO @consumerIdVar FROM namedEntities.consumers WHERE name = 'test';
 
 /* Seed Individual Entity */
 
+SELECT 1 INTO @individualNedId;
+
 INSERT INTO namedEntities.namedEntityIdentifiers(id,typeId)
-  VALUES (1, (select gt.id from namedEntities.globalTypes gt
+  VALUES (@individualNedId, (select gt.id from namedEntities.globalTypes gt
                 join namedEntities.typeDescriptions td on gt.typeid = td.id
                where td.description='Named Party Types' and gt.shortDescription='Individual'));
+
+--SELECT id INTO @individualNedId FROM namedEntities.namedEntityIdentifiers WHERE id = 1;
 
 SELECT gt.id INTO @srcTypeIdVar
   FROM namedEntities.globalTypes gt
@@ -17,18 +21,18 @@ SELECT gt.id INTO @srcTypeIdVar
  WHERE td.description='Source Applications' AND gt.shortDescription='Ambra';
 
 INSERT INTO namedEntities.individualProfiles (id, nedId, firstName, lastName, displayName, biography, isActive, createdBy, lastModifiedBy, sourceTypeId)
-  VALUES (1,1,'Cosmo','Kramer','ckramer','a short bio',1, @consumerIdVar, @consumerIdVar, @srcTypeIdVar);
+  VALUES (1,@individualNedId,'Cosmo','Kramer','ckramer','a short bio',1, @consumerIdVar, @consumerIdVar, @srcTypeIdVar);
 
 INSERT INTO namedEntities.emails (id, nedId, emailAddress, createdBy, lastModifiedBy, sourceTypeId)
-    VALUES (1,1, 'ckramer@plos.org', @consumerIdVar, @consumerIdVar, @srcTypeIdVar);
+    VALUES (1,@individualNedId, 'ckramer@plos.org', @consumerIdVar, @consumerIdVar, @srcTypeIdVar);
 
 INSERT INTO namedEntities.authCas (id, nedId, emailId, authId, password, createdBy, lastModifiedBy)
-    VALUES (1,1,1, UUID(),  /* password = "password1" */
+    VALUES (1,@individualNedId,1, UUID(),  /* password = "password1" */
             'f953d98b896b3739bf925346cffb4e2e9b5e724d14bc66bc6562672944ae21d1f76bf904416af9ce52ddff3d9b46b447a4d1d6e17318299e967d0362d0df0ad6',
             @consumerIdVar, @consumerIdVar);
 
 INSERT INTO namedEntities.uniqueIdentifiers (id, nedId, typeId, uniqueIdentifier, sourceTypeId, createdBy, lastModifiedBy)
-    VALUES (1, 1,
+    VALUES (1, @individualNedId,
         (select gt.id from namedEntities.globalTypes gt
            join namedEntities.typeDescriptions td on gt.typeid = td.id
           where td.description='UID Individual Types' and gt.shortDescription='Ambra'),
@@ -36,13 +40,16 @@ INSERT INTO namedEntities.uniqueIdentifiers (id, nedId, typeId, uniqueIdentifier
 
 /* Seed Organization Entity */
 
+SELECT 2 INTO @orgNedId;
+
 INSERT INTO namedEntities.namedEntityIdentifiers(id,typeId)
-  VALUES (2, (select gt.id from namedEntities.globalTypes gt
+  VALUES (@orgNedId, (select gt.id from namedEntities.globalTypes gt
                 join namedEntities.typeDescriptions td on gt.typeid = td.id
                where td.description='Named Party Types' and gt.shortDescription='Organization'));
 
+
 INSERT INTO  namedEntities.organizations (id, nedId, familiarName, legalName, isActive, createdBy, lastModifiedBy, sourceTypeId)
-  VALUES (1,2,'ABC''s Inc (FN)','ABC''s Inc (LN)',1,1,1,
+  VALUES (1,@orgNedId,'ABC''s Inc (FN)','ABC''s Inc (LN)',1,1,1,
     (select gt.id from namedEntities.globalTypes gt
        join namedEntities.typeDescriptions td on gt.typeid = td.id
       where td.description='Source Applications' and gt.shortDescription='Ambra'));
