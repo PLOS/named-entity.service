b4e0f4fcce71be237bdc410c5827f96580840dac
Matthew Bowen <mbowen@plos.org>
2017-01-09 15:46:17 -0800

    PLT-614: update execution of ringgold db exists check.

--- bin/install.sh
+++ bin/install.sh
@@ -30,7 +30,7 @@ echo -e "==> Importing Ringgold Database ($ringgold_gz)\n"
 ringgold_db_sql="
   select count(*) from information_schema.tables
     where lower(table_schema)='ringgold' and lower(table_name)='institutions'"
-ringgold_db_exist=$(exec_sql "$(ringgold_db_sql)")
+ringgold_db_exist=$(exec_sql "$ringgold_db_sql")
 
 if [ $ringgold_db_exist -eq 0 ]; then
     mysql -user=${NED_DB_USER} -password=${NED_DB_PASSWORD} < $ringgold_gz
@@ -52,10 +52,6 @@ echo -e "==> Deploying WAR ($ned_war)\n"
 #rm -f ${NED_WEBAPPS}/v1.war
 cp -f $ned_war ${NED_ROOT}/webapps/v1.war
 #sudo service ned start
-
-# OR deploy with tomcat manager
-#curl -T "named-entity-service*.war" "http://tomcat:s3cret@localhost:8080/manager/text/deploy?path=/v1&update=true"
-
 
 echo "Started  : $T1"
 echo "Finished : `date`"
